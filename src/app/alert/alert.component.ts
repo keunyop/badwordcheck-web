3701f0d7db0cfadc2ece70a87bf0917a1b1f832a
keunyop <kylee1112@hotmail.com>
2020-08-30 07:39:30 +0900

    developing

--- src/app/alert/alert.component.ts
+++ src/app/alert/alert.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertDataService } from '../service/data/alert-data.service';
+import { Alert } from '../document/document.component';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-alert',
   templateUrl: './alert.component.html',
-  styleUrls: ['./alert.component.css']
+  styleUrls: ['./alert.component.css'],
 })
 export class AlertComponent implements OnInit {
+  id: number;
+  alert: Alert;
 
-  constructor() { }
+  constructor(
+    private alertService: AlertDataService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+    this.alert = new Alert(1, '', false, new Date());
+    this.alertService
+      .retrieveAlert('kevin', this.id)
+      .subscribe((data) => (this.alert = data));
   }
 
+  saveAlert() {}
 }
--- src/app/service/data/alert-data.service.ts
+++ src/app/service/data/alert-data.service.ts
@@ -6,9 +6,7 @@ import { Alert } from 'src/app/document/document.component';
   providedIn: 'root',
 })
 export class AlertDataService {
-  constructor(
-    private http:HttpClient
-  ) {}
+  constructor(private http: HttpClient) {}
 
   retrieveAllAlerts(username) {
     return this.http.get<Alert[]>(
@@ -16,7 +14,15 @@ export class AlertDataService {
     );
   }
 
+  retrieveAlert(username, id) {
+    return this.http.get<Alert>(
+      `http://localhost:8081/users/${username}/alerts/${id}`
+    );
+  }
+
   deleteAlert(username, id) {
-    return this.http.delete(`http://localhost:8081/users/${username}/alerts/${id}`)
+    return this.http.delete(
+      `http://localhost:8081/users/${username}/alerts/${id}`
+    );
   }
 }
